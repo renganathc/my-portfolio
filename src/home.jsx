1a803cf871dfdba8ff0cdee4ca019388af6ee0a2
Renganath Chokkalingam <renganathc@Renganaths-MacBook-Air.local>
2025-06-09 12:07:35 +0530

    Updated profile pic

--- src/home.jsx
+++ src/home.jsx
@@ -10,7 +10,7 @@ function Home() {
         <div className="home">
 
             <div className="img-holder">
-                <img className='main-img' src="/5.jpg" />
+                <img className='main-img' src="/headshot2.jpg" />
             </div>
 
             <div className="name-holder">
